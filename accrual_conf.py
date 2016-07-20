3f3e04a6eb19f41b3d184e115398ae905d81f08b
Jason Brooks <jason.brooks@greenphire.com>
2016-07-20 07:02:35 -0400

    added conf file

---
+++ accrual_conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+DB_NAME = 'accrual_payments.db'
+TABLE_NAME = 'customer_invoices'
+
+# percentage of cash back a customer gets for each invoice
+CASH_BACK_PERCENTAGE = 5.0
+
+# total number of invoices a customer must have to qualify for accrual payment
+ACCRUAL_INVOICE_PAYOUT = 5
